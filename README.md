981eebc97d257b7566f95e8cacaf6414a6db5f46
Fernando Pimenta <fernando.pimenta107@gmail.com>
2019-09-30 22:28:40 -0300

    publicando lambda

--- README.md
+++ README.md
@@ -2,5 +2,54 @@ pip3 install -r requirements.txt
 # pra gerar o requirements.txt
 pip3 freeze > requirements.txt
 
+aws s3 mb s3://fernando.pimenta.com.br
 aws s3 sync . s3://fernando.pimenta.com.br
 aws rekognition create-collection --collection-id nome-da-colecao
+
+# deletando ambiente para evitar custos
+aws s3 rb s3://fernando.pimenta.com.br --force
+aws rekognition delete-collection --collection-id faces
+
+# s3 subir site
+aws s3 rm s3://fernando.pimenta.site.com.br --recursive
+aws s3 cp ./fa-site s3://fernando.pimenta.site.com.br --recursive --acl public-read
+aws s3 cp teste.png s3://fernando.pimenta.site.com.br
+# Cors
+<?xml version="1.0" encoding="UTF-8"?>
+<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+    <CORSRule>
+        <AllowedOrigin>*</AllowedOrigin>
+        <AllowedMethod>GET</AllowedMethod>
+        <AllowedMethod>HEAD</AllowedMethod>
+        <MaxAgeSeconds>3000</MaxAgeSeconds>
+        <AllowedHeader>Authorization</AllowedHeader>
+    </CORSRule>
+</CORSConfiguration> 
+
+# policy
+{
+    "Id": "Policy1569889808748",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1569889807410",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::fernando.pimenta.com.br/*",
+            "Principal": "*"
+        }
+    ]
+}
+
+#subindo lambda
+zip faceanalise.zip faceanalise.py
+aws lambda update-function-code 
+--function-name faceAnalise --zip-file fileb://faceanalise.zip
+
+#publicando
+aws lambda publish-version --function-name faceAnalise
+aws lambda create-alias --function-name faceAnalise --function-version
+1 --name PROD
+
--- faceanalise.py
+++ faceanalise.py
@@ -24,7 +24,47 @@ def cria_lista_faceId_detectadas(faces_detectadas):
         faceId_detectadas.append(faces_detectadas['FaceRecords'][i]['Face']['FaceId'])
     return faceId_detectadas
 
-faces_detectadas = detecta_faces()
-faceId_detectadas = cria_lista_faceId_detectadas(faces_detectadas)
-print(faceId_detectadas)
-# print(json.dumps(faces_detectadas))
+def compara_imagens(faceId_detectadas):
+    resultado_comparacao = []
+    for ids in faceId_detectadas:
+        resultado_comparacao.append(
+            client.search_faces(
+                CollectionId='faces',
+                FaceId=ids,
+                FaceMatchThreshold=80,
+                MaxFaces=10
+            )
+        )
+    return resultado_comparacao
+
+def gera_dados_json(resultado_comparacao):
+    dados_json = []
+    for face_matches in resultado_comparacao:
+        if len(face_matches.get('FaceMatches')) >= 1:
+            perfil = dict(nome=
+                          face_matches['FaceMatches'][0]['Face']['ExternalImageId'],
+                          faceMatch=
+                          round(face_matches['FaceMatches'][0]['Similarity'], 2)
+                          )
+            dados_json.append(perfil)
+    return dados_json
+
+def publica_dados(dados_json):
+    arquivo = s3.Object('fernando.pimenta.site.com.br', 'dados.json')
+    arquivo.put(Body=json.dumps(dados_json))
+
+def exclui_imagem_colecao(faceId_detectadas):
+    client.delete_faces(
+        CollectionId='faces',
+        FaceIds=faceId_detectadas
+    )
+
+def main(event, context):
+    faces_detectadas = detecta_faces()
+    faceId_detectadas = cria_lista_faceId_detectadas(faces_detectadas)
+    resultado_comparacao = compara_imagens(faceId_detectadas)
+    dados_json = gera_dados_json(resultado_comparacao)
+    publica_dados(dados_json)
+    exclui_imagem_colecao(faceId_detectadas)
+    print(json.dumps(dados_json))
+    # print(json.dumps(faces_detectadas))
